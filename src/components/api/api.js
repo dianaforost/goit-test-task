cd0bfa842a4d5dea4cb39ed50425bbf0c222b249
Diana Forostiana <dianaforost@gmail.com>
2023-06-04 19:42:00 +0200

    Added
    Added a back button on the page. Updated backend requests. Added sorting and styles to them.

--- src/components/api/api.js
+++ src/components/api/api.js
@@ -1,27 +1,33 @@
 import axios from "axios";
 axios.defaults.baseURL="https://6477b610362560649a2ce251.mockapi.io"
-export const fetchUsers = async() => {
-    const response = await axios.get("/users");
-    return response.data;
-}
-export const fetchPaginationUsers = async(page,limit) => {
+export const fetchUsers = async(page,limit, filterOption) => {
     const response = await axios.get("/users", {
         params: {
-          page: page,
-          limit: limit,
+            page: page,
+            limit: limit,
+            filter: filterOption,
         },
       });
   return response.data;
 }
-export const followUser = async(id, isFollowed) =>{
+export const followUser = async(id) =>{
     const response = await axios.get(`/users/${id}`);
     const user = response.data;
     const updatedData = {
         ...user,
-        followers: user.followers + 1,
-        isFollowed: true,
+        followers: user.followers + (user.isFollowed ? -1 : 1),
+        isFollowed: user.isFollowed ? false : true,
       };
       const updateResponse = await axios.put(`/users/${id}`, updatedData);
 
       return updateResponse.data;
+}
+export const fetchSortBy = async(filterOption) =>{
+    const response = await axios.get("/users", {
+        params: {
+            filter: filterOption,
+        },
+      });
+     
+      return response.data;
 }
